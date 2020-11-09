bdfc27f1131ce7480d234af48efae3d9eb9cae75
Jagrati Joshi <jagrati.joshi@outlook.com>
2020-11-08 23:26:22 -0700

    Included comments

--- EmployeeSQL/CreateTables.sql
+++ EmployeeSQL/CreateTables.sql
@@ -1,17 +1,25 @@
+--Dropping table from database if exist
 drop table departments;
 
+--Creating Departments table as per data from csv file
 create table departments (
 	dept_no varchar(10) primary key not null
 	,dept_name text
 );
 
+--Dropping table from database if exist
 drop table titles;
+
+--Creating Employee Titles table as per data from csv file
 create table titles (
 	title_id varchar(10) primary key not null
 	,title text
 );
 
+--Dropping table from database if exist
 drop table employees;
+
+--Creating Employees table as per data from csv file
 create table employees (
 	emp_no int primary key not null
 	,emp_title_id varchar(10) 
@@ -22,19 +30,28 @@ create table employees (
 	,hire_date date
 );
 
+--Dropping table from database if exist
 drop table dept_emp;
+
+--Creating Department Employees table as per data from csv file
 create table dept_emp(
 	emp_no int
 	,dept_no varchar(10)
 );
 
+--Dropping table from database if exist
 drop table dept_manager;
+
+--Creating Department Managers table as per data from csv file
 create table dept_manager(
 	dept_no varchar(10)
 	,emp_no int
 );
 
+--Dropping table from database if exist
 drop table salaries;
+
+--Creating Employee Salary table as per data from csv file
 create table salaries(
 	emp_no int
 	,salary float
--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -1,3 +1,4 @@
+--Getting list of employee number, last name, first name, sex, and salary
 select 
 	e.emp_no
 	,e.last_name
@@ -7,6 +8,7 @@ select
 from employees e
 	join salaries s on e.emp_no = s.emp_no;
 
+--Getting list first name, last name, and hire date for employees who were hired in 1986
 select
 	e.first_name
 	,e.last_name
@@ -14,6 +16,7 @@ select
 from employees e
 where extract(year from e.hire_date) = 1986;
 
+--Getting list of department number, department name, department manager's employee number, last name, first name
 select
 	dm.dept_no
 	,d.dept_name
@@ -24,6 +27,7 @@ from dept_manager dm
 join departments d on d.dept_no = dm.dept_no
 join employees e on e.emp_no = dm.emp_no;
 
+--Getting list of employee number, last name, first name, and department name
 select
 	de.emp_no
 	,e.last_name
@@ -33,6 +37,7 @@ from dept_emp de
 join departments d on d.dept_no = de.dept_no
 join employees e on e.emp_no = de.emp_no;
 
+--Getting list of first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B"
 select
 	e.first_name
 	,e.last_name
@@ -40,7 +45,8 @@ select
 from employees e
 where e.first_name = 'Hercules'
 	and left(e.last_name,1) = 'B';
-	
+
+--Getting list of all employees in the Sales department, including their employee number, last name, first name, and department name
 select
 	de.emp_no
 	,e.last_name
@@ -51,6 +57,7 @@ join departments d on d.dept_no = de.dept_no
 join employees e on e.emp_no = de.emp_no
 where d.dept_name = 'Sales';
 
+--Getting list of all employees in the Sales department and Development department, including their employee number, last name, first name, and department name
 select
 	de.emp_no
 	,e.last_name
@@ -61,15 +68,10 @@ join departments d on d.dept_no = de.dept_no
 join employees e on e.emp_no = de.emp_no
 where d.dept_name in ('Sales', 'Development');
 
+--Getting list of the frequency count of employee last names, i.e., how many employees share each last name, in descending order of the frequency
 select
 	e.last_name
 	,count(*) as frequency
 from employees e
 group by e.last_name
 order by frequency desc;
-
-
-
-
-
-
